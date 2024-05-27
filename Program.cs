b448884d8f9b4b7b01197ffd6e48619c5ddaac8d
clement <clement.vandewalle@adeo.com>
2024-05-27 13:07:17 +0200

    Implémentation de la logique principale de l'application

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using AirQualityApp.Services;
+
+namespace AirQualityApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: AirQualityApp <country>");
+                return;
+            }
+
+            string country = args[0];
+
+            var cityService = new CityService();
+            var airQualityService = new AirQualityService();
+
+            var cities = await cityService.GetCitiesAsync(country);
+            var airQualities = await airQualityService.GetAirQualityAsync(cities);
+
+            var sortedCities = airQualities.OrderBy(a => a.QualityIndex).Take(15);
+
+            foreach (var city in sortedCities)
+            {
+                Console.WriteLine($"{city.City}: {city.QualityIndex}");
+            }
+        }
+    }
+}
