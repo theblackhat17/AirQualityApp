62ebbea2b0956dece4e0363a05f8321d4c558259
clement <clement.vandewalle@adeo.com>
2024-05-27 13:05:23 +0200

    Implémentation de la classe CityService pour récupérer les villes

---
+++ Services/CityService.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AirQualityApp.Models;
+using Newtonsoft.Json.Linq;
+
+namespace AirQualityApp.Services
+{
+    public class CityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _geoNamesUsername = "cvandewalle";
+
+        public CityService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<City>> GetCitiesAsync(string country)
+        {
+            var response = await _httpClient.GetStringAsync($"http://api.geonames.org/searchJSON?formatted=true&q=city&maxRows=15&country={country}&username={_geoNamesUsername}");
+            var citiesData = JObject.Parse(response)["geonames"];
+            var cities = new List<City>();
+
+            foreach (var cityData in citiesData)
+            {
+                cities.Add(new City
+                {
+                    Name = cityData["name"].ToString(),
+                    Latitude = (double)cityData["lat"],
+                    Longitude = (double)cityData["lng"]
+                });
+            }
+
+            return cities;
+        }
+    }
+}
