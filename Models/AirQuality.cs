e5742489d17669129c007c9398e9323762e61921
clement <clement.vandewalle@adeo.com>
2024-05-27 13:06:56 +0200

    Implémentation de la classe AirQualityService pour récupérer les données de qualité de l'air

---
+++ Models/AirQuality.cs
@@ -0,0 +1,8 @@
+namespace AirQualityApp.Models
+{
+    public class AirQuality
+    {
+        public string City { get; set; }
+        public int QualityIndex { get; set; }
+    }
+}
---
+++ Services/AirQualityService.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AirQualityApp.Models;
+using Newtonsoft.Json.Linq;
+
+namespace AirQualityApp.Services
+{
+    public class AirQualityService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "d2dbcf9ede6b18f216459d3c829d2b21";
+
+        public AirQualityService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<AirQuality>> GetAirQualityAsync(List<City> cities)
+        {
+            var airQualities = new List<AirQuality>();
+
+            foreach (var city in cities)
+            {
+                var response = await _httpClient.GetStringAsync($"http://api.openweathermap.org/data/2.5/air_pollution?lat={city.Latitude}&lon={city.Longitude}&appid={_apiKey}");
+                var airQualityData = JObject.Parse(response)["list"][0]["main"];
+                airQualities.Add(new AirQuality
+                {
+                    City = city.Name,
+                    QualityIndex = (int)airQualityData["aqi"]
+                });
+            }
+
+            return airQualities;
+        }
+    }
+}
